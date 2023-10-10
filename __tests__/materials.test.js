94d75a31ded3654d9e65fc37058163403d7c240c
juligarc91 <julissagarcia@pursuit.org>
2023-10-10 18:13:16 -0400

    Added test case for inputs equal to or less than 0

--- __tests__/materials.test.js
+++ __tests__/materials.test.js
@@ -1,7 +1,7 @@
 const { generateRandomMaterials } = require ("../materials");
 
 describe(generateRandomMaterials, () => {
-    test("input is a number data type", () => {
+    test("input is a number data type greater than 0", () => {
         const input = 2; // number type
         const actual = generateRandomMaterials(input);
 
@@ -9,7 +9,7 @@ describe(generateRandomMaterials, () => {
         expect(typeof actual).toBe("string");
 
         // Assert that actual result is not empty
-        expect(actual.length).toBeGreaterThan(0);
+//        expect(actual.length).toBeGreaterThan(0);
     });
     test ("generates correct number of items",() => {
         const input = 5; // test for 5 materials
@@ -34,4 +34,26 @@ describe(generateRandomMaterials, () => {
         expect(actualArray).toBeUndefined();
         expect(actualObject).toBeUndefined();
     });
+    test("returns undefined if number is less than or equal to 0", () => {
+
+        // setting up a scenario where negative number is input.
+        const inputNegative = -3; 
+    
+        // call function "generateRandomMaterials" with this negative number.
+        // "actualNegative" will hold whatever the function returns for this negative input.
+        const actualNegative = generateRandomMaterials(inputNegative); 
+    
+        // setting up another scenario where we provide zero as input.
+        // "inputZero" is test value that we choose to be zero.
+        const inputZero = 0; 
+    
+        // call function with this zero input.
+        // "actualZero" will hold whatever the function returns for this input of zero.
+        const actualZero = generateRandomMaterials(inputZero); 
+    
+        // checking that in both cases - for the negative input and the zero input - 
+        // function should return 'undefined'. If it doesn't, the test will fail.
+        expect(actualNegative).toBeUndefined(); 
+        expect(actualZero).toBeUndefined(); 
+    });    
 });
--- materials.js
+++ materials.js
@@ -1,7 +1,14 @@
 const { faker } = require("@faker-js/faker");
 
+/**
+ * Generates a comma-separated string of random materials.
+ * @function
+ * @param {number} num - The number of random materials to generate.
+ * @returns {string|undefined} A comma-separated formatted string of random materials, or undefined if the provided argument is not a number.
+ * @example Input: 4 Output: "Frozen, Plastic, Metal, Fresh"
+ */
 const generateRandomMaterials = (num) => {
-    if (typeof num !== "number") return undefined;
+    if (typeof num !== "number" || num <= 0) return undefined;
         let output = [];
         for (let i = 0; i < num; i++) {
             const material = {
