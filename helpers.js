f16c925088632e6e15309928f422dde9860f2c0c
armandopires <armandopires@pursuit.org>
2023-10-10 22:40:33 -0400

    Updated work

--- helpers.js
+++ helpers.js
@@ -16,32 +16,3 @@ module.exports = {
 };
 
 
-// const fs = require('fs');
-
-// function ensureDirectoryExists(directory) {
-//   if (!fs.existsSync(directory)) {
-//     fs.mkdirSync(directory, { recursive: true });
-//   }
-// }
-
-// function writeJSONFile(path, fileName, data) {
-//   ensureDirectoryExists(path); // Ensure the directory exists
-//   data = JSON.stringify(data, null, 2);
-//   return fs.writeFileSync(`${path}/${fileName}`, data, { encoding: 'utf-8' });
-// }
-
-// function readJSONFile(path, fileName) {
-//   const filePath = `${path}/${fileName}`;
-
-//   if (fs.existsSync(filePath)) {
-//     const collection = fs.readFileSync(filePath, 'utf-8');
-//     return collection ? JSON.parse(collection) : [];
-//   } else {
-//     return [];
-//   }
-// }
-
-// module.exports = {
-//   writeJSONFile,
-//   readJSONFile,
-// };
